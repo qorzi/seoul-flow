1f033d13478c1596e15ae866fda574e068afee0f
qorzi <qorzi00@gmail.com>
2025-06-22 19:12:15 +0900

    backend project init

---
+++ backend/src/main/java/com/demo/seoul_flow/SeoulFlowApplication.java
@@ -0,0 +1,13 @@
+package com.demo.seoul_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/demo/seoul_flow/SeoulFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.seoul_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
